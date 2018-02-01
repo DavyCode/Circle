3388e1eebb835d79751cff1592504c6858abb4fb
Davy'd <pdaswift3009@gmail.com>
2018-02-01 21:33:43 +0100

    setup server with middleware

--- controllers/users.js
+++ controllers/users.js
@@ -4,10 +4,14 @@ module.exports = function(_) {
   return  {
     SetRouting : function(router) {
       router.get('/', this.indexPage);
+      router.get('/signup', this.signUp);
     },
 
     indexPage: function(req, res) {
       return res.render('index', {test: "Hello from the server side"})
+    },
+    signUp: function(req, res) {
+      return res.render('auth/signup');
     }
   }
 }
