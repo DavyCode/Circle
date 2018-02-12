17628ffcf339b0ba5cdd36aed0efa7c213f2e499
Davy'd <pdaswift3009@gmail.com>
2018-02-12 22:23:11 +0100

    display request data

--- controllers/group.js
+++ controllers/group.js
@@ -1,63 +1,76 @@
-
-module.exports = function(Users, async){
+module.exports = function(Users, async) {
     return {
-        SetRouting: function(router){
-              router.get('/group/:name', this.getGroupPage);
-             
-              router.post('/group/:name', this.postGroupPage);
+        SetRouting: function(router) {
+            router.get('/group/:name', this.getGroupPage);
+
+            router.post('/group/:name', this.postGroupPage);
         },
 
-        getGroupPage: function(req, res){
+        getGroupPage: function(req, res) {
             const name = req.params.name;
 
             // Group.findById(group, (err, foundGroup) => {
             //     (err)? console.log(err):  res.render('group/group', {title: 'Circle - Group Name', user: req.user, group: foundGroup});
             // });    
-            
-            res.render('group/group', {title: 'Circle - Group Name', user: req.user, groupName: name});
+
+            async.parallel([
+                function(callback) {
+                    Users.findOne({ 'username': req.user.username })
+                        .populate('request.userId')
+                        .exec((err, result) => {
+                            callback(err, result);
+                        })
+                }
+            ], (err, results) => {
+                const requestData = results[0];
+
+                res.render('group/group', { title: 'Circle - Group Name', user: req.user, groupName: name, data: requestData });
+            });
+
         },
 
-        postGroupPage: function(req,res){
-            async.parallel([ 
-                function(callback){
-                    if(req.body.receiver){
+        postGroupPage: function(req, res) {
+            async.parallel([
+                function(callback) {
+                    if (req.body.receiver) {
                         Users.update({
-                            'username': req.body.receiver,
-                            'request.userId': {$ne: req.user._id},
-                            'friendsList.friendId': {$ne: req.user._id}
-                        },
-                        {
-                            $push: {request: {
-                                userId: req.user._id,
-                                username: req.user.username
-                            }},
-                            $inc: { totalRequest: 1}
+                                'username': req.body.receiver,
+                                'request.userId': { $ne: req.user._id },
+                                'friendsList.friendId': { $ne: req.user._id }
+                            }, {
+                                $push: {
+                                    request: {
+                                        userId: req.user._id,
+                                        username: req.user.username
+                                    }
+                                },
+                                $inc: { totalRequest: 1 }
 
-                        }, (err, count) => {
-                            callback(err, count)
-                        }
-                    
-                    )
+                            }, (err, count) => {
+                                callback(err, count)
+                            }
+
+                        )
                     }
-                }
-                ,
-                function(callback){
-                    if(req.body.receiver){
+                },
+                function(callback) {
+                    if (req.body.receiver) {
                         Users.update({
-                                    'username': req.user.username,
-                                    'sentRequest.username': {$ne: req.body.receiver}
-                            },
-                            {
-                              $push: {sentRequest: {
-                                        username: req.body.receiver
-                            }}
-                            },(err, count) => {
-                                    callback(err, count);
-                            })
+                            'username': req.user.username,
+                            'sentRequest.username': { $ne: req.body.receiver }
+                        }, {
+                            $push: {
+                                sentRequest: {
+                                    username: req.body.receiver
+                                }
+                            }
+                        }, (err, count) => {
+                            callback(err, count);
+                        })
                     }
                 }
             ], (err, results) => {
-                res.redirect('/group/'+req.params.name);
+                res.redirect('/group/' + req.params.name);
             });
         }
 
